502f4061bd9a6330e4ea0210561976c345dc2763
hardeek-sharma <hardeeksa@gmail.com>
2025-12-24 20:08:12 -0500

    Add loop and variable to create divs

---
+++ javascript.js
@@ -0,0 +1,7 @@
+let container = document.querySelector('#container');
+let ratio = 100;
+
+for (i = 0; i < ratio**2; i++) {
+  let divs = document.createElement('div');
+  container.appendChild(divs)
+}
