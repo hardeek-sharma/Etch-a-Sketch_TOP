ccf7e0ffbdd9e5a7b06d1b262edccd51e6dc33d6
hardeek-sharma <hardeeksa@gmail.com>
2025-12-24 22:31:38 -0500

    Add eventListener for hover and actions

--- javascript.js
+++ javascript.js
@@ -1,7 +1,15 @@
 let container = document.querySelector('#container');
-let ratio = 100;
+let ratio = 16;
 
 for (i = 0; i < ratio**2; i++) {
-  let divs = document.createElement('div');
-  container.appendChild(divs)
+  container.appendChild(document.createElement('div')).classList.add('box');
 }
+let boxes = document.querySelectorAll('.box');
+
+boxes.forEach((box) => {
+  box.style.width = `${100/ratio}%`;
+  box.style.height = `${100/ratio}%`;
+  box.addEventListener('mouseover', (e) => {
+    box.classList.add('hovered');
+  })
+})
