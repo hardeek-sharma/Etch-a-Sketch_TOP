39ece4c0f46042aa29993d6150292f4e461f050b
hardeek-sharma <hardeeksa@gmail.com>
2025-12-24 23:37:56 -0500

    Add functions to make button work

--- javascript.js
+++ javascript.js
@@ -1,16 +1,38 @@
 let container = document.querySelector('#container');
 let sizeBtn = document.querySelector('#size-button');
 let ratio = 16;
+let boxes;
 
-for (i = 0; i < ratio**2; i++) {
-  container.appendChild(document.createElement('div')).classList.add('box');
+function makeGrid(ratio) {
+  for (i = 0; i < ratio**2; i++) {
+    container.appendChild(document.createElement('div')).classList.add('box', i);
+  }
 }
-let boxes = document.querySelectorAll('.box');
 
-boxes.forEach((box) => {
-  box.style.width = `${100/ratio}%`;
-  box.style.height = `${100/ratio}%`;
-  box.addEventListener('mouseover', (e) => {
-    box.classList.add('hovered');
-  })
+makeGrid(ratio);
+
+function addGridActions() {
+  boxes = document.querySelectorAll('.box');
+
+  boxes.forEach((box) => {
+    box.style.width = `${100/ratio}%`;
+    box.style.height = `${100/ratio}%`;
+    box.addEventListener('mouseover', (e) => {
+      box.classList.add('hovered');
+    })
+  });
+}
+addGridActions();
+
+sizeBtn.addEventListener('click', (e) => {
+  let newSize = Number(prompt('Pick Size(2-100)')); 
+  if (newSize >= 2 && newSize <= 100) {
+    ratio = newSize;
+    boxes.forEach((box) => {
+      box.remove();
+    })
+
+    makeGrid(newSize);
+    addGridActions();
+  }
 })
